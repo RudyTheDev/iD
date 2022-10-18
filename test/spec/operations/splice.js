acd9cc3dc8eda60ab35b2c6fe0c8f4a6f7533021
RudyTheDev <3857299+RudyTheDev@users.noreply.github.com>
2022-10-18 21:38:37 +0300

    Add splice operation unit tests to for being disabled on non-area; fixes

--- test/spec/operations/splice.js
+++ test/spec/operations/splice.js
@@ -6,65 +6,110 @@ describe('iD.operationSplice', function () {
     fakeContext = {};
     fakeContext.graph = function() { return graph; };
 
-    beforeEach(function () {
-        //
-        // Situation:
-        //    b ---> c
-        //    ^ \    |
-        //    |    \ |
-        //    a <--- d
-        //
-        //    Area a-b-c-d-a
-        //    Cut line b-d; has tags
+    describe('valid geometry - area', function () {
 
-        var a = iD.osmNode({ id: 'a', loc: [0, 0] });
-        var b = iD.osmNode({ id: 'b', loc: [0, 1] });
-        var c = iD.osmNode({ id: 'c', loc: [1, 1] });
-        var d = iD.osmNode({ id: 'd', loc: [0, 1] });
+        beforeEach(function () {
+            //
+            // Situation:
+            //    b ---> c
+            //    ^ \    |
+            //    |    \ |
+            //    a <--- d
+            //
+            //    Area a-b-c-d-a
+            //    Cut line b-d
 
-        graph = iD.coreGraph([
-            a, b, c, d,
-            iD.osmWay({ id: 'area', nodes: ['a', 'b', 'c', 'd', 'a'], tags: { area: 'yes' } }),
-            iD.osmWay({ id: 'cutline', nodes: ['b', 'd'], tags: { interesting: 'yes' } })
-        ]);
+            var a = iD.osmNode({ id: 'a', loc: [0, 0] });
+            var b = iD.osmNode({ id: 'b', loc: [0, 1] });
+            var c = iD.osmNode({ id: 'c', loc: [1, 1] });
+            var d = iD.osmNode({ id: 'd', loc: [0, 1] });
+
+            graph = iD.coreGraph([
+                a, b, c, d,
+                iD.osmWay({ id: 'area', nodes: ['a', 'b', 'c', 'd', 'a'], tags: { area: 'yes' } }),
+                iD.osmWay({ id: 'cutline', nodes: ['b', 'd'] })
+            ]);
+        });
+
+        describe('#disabled', function () {
+
+            it('is not available for no selected ids', function () {
+                expect(iD.operationSplice(fakeContext, []).available()).to.be.not.ok;
+            });
+
+            it('is not available for selected area', function () {
+                expect(iD.operationSplice(fakeContext, [ 'area' ]).available()).to.be.not.ok;
+            });
+
+            it('is not available for selected cutline, area and some node', function () {
+                expect(iD.operationSplice(fakeContext, [ 'area', 'cutline', 'a' ]).available()).to.be.not.ok;
+            });
+
+            it('is not available for selected area and node', function () {
+                expect(iD.operationSplice(fakeContext, [ 'area', 'b' ]).available()).to.be.not.ok;
+            });
+
+            it('is not available for selected cutline\'s node', function () {
+                expect(iD.operationSplice(fakeContext, [ 'b', 'd' ]).available()).to.be.not.ok;
+            });
+
+            it('is not available for selected area and cutline\'s node', function () {
+                expect(iD.operationSplice(fakeContext, [ 'area', 'b', 'd' ]).available()).to.be.not.ok;
+            });
+        });
+
+        describe('#available', function () {
+
+            it('is available for selected cutline', function () {
+                expect(iD.operationSplice(fakeContext, [ 'cutline' ]).available()).to.be.ok;
+            });
+
+            it('is available for selected cutline and area', function () {
+                expect(iD.operationSplice(fakeContext, [ 'cutline', 'area' ]).available()).to.be.ok;
+                expect(iD.operationSplice(fakeContext, [ 'area', 'cutline' ]).available()).to.be.ok;
+            });
+        });
     });
 
-    describe('#disabled', function () {
+    describe('invalid geometry - loop', function () {
 
-        it('is not available for no selected ids', function () {
-            expect(iD.operationSplice(fakeContext, []).available()).to.be.not.ok;
+        beforeEach(function () {
+            //
+            // Situation:
+            //    b ---> c
+            //    ^ \    |
+            //    |    \ |
+            //    a <--- d
+            //
+            //    Loop (closed way but not area) a-b-c-d-a
+            //    Cut line b-d
+
+            var a = iD.osmNode({ id: 'a', loc: [0, 0] });
+            var b = iD.osmNode({ id: 'b', loc: [0, 1] });
+            var c = iD.osmNode({ id: 'c', loc: [1, 1] });
+            var d = iD.osmNode({ id: 'd', loc: [0, 1] });
+
+            graph = iD.coreGraph([
+                a, b, c, d,
+                iD.osmWay({ id: 'loop', nodes: ['a', 'b', 'c', 'd', 'a'] }),
+                iD.osmWay({ id: 'cutline', nodes: ['b', 'd'], tags: { interesting: 'yes' } })
+            ]);
         });
 
-        it('is not available for selected area', function () {
-            expect(iD.operationSplice(fakeContext, [ 'area' ]).available()).to.be.not.ok;
-        });
+        describe('#disabled', function () {
 
-        it('is not available for selected cutline, area and some node', function () {
-            expect(iD.operationSplice(fakeContext, [ 'area', 'cutline', 'a' ]).available()).to.be.not.ok;
-        });
+            it('is not available for selected cutline', function () {
+                expect(iD.operationSplice(fakeContext, [ 'cutline' ]).available()).to.be.not.ok;
+            });
 
-        it('is not available for selected area and node', function () {
-            expect(iD.operationSplice(fakeContext, [ 'area', 'b' ]).available()).to.be.not.ok;
-        });
+            it('is available for selected cutline and area', function () {
+                expect(iD.operationSplice(fakeContext, [ 'cutline', 'loop' ]).available()).to.be.not.ok;
+                expect(iD.operationSplice(fakeContext, [ 'loop', 'cutline' ]).available()).to.be.not.ok;
+            });
 
-        it('is not available for selected cutline\'s node', function () {
-            expect(iD.operationSplice(fakeContext, [ 'b', 'd' ]).available()).to.be.not.ok;
-        });
-
-        it('is not available for selected area and cutline\'s node', function () {
-            expect(iD.operationSplice(fakeContext, [ 'area', 'b', 'd' ]).available()).to.be.not.ok;
-        });
-    });
-
-    describe('#available', function () {
-
-        it('is available for selected cutline', function () {
-            expect(iD.operationSplice(fakeContext, [ 'cutline' ]).available()).to.be.ok;
-        });
-
-        it('is available for selected cutline and area', function () {
-            expect(iD.operationSplice(fakeContext, [ 'cutline', 'area' ]).available()).to.be.ok;
-            expect(iD.operationSplice(fakeContext, [ 'area', 'cutline' ]).available()).to.be.ok;
+            it('is not available for selected area', function () {
+                expect(iD.operationSplice(fakeContext, [ 'loop' ]).available()).to.be.not.ok;
+            });
         });
     });
 });
